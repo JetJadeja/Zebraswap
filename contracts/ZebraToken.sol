2db04c5b5b461f6d70a9ef50d7a62ad2f1585b18
Jet <jetjadeja@gmail.com>
2020-11-20 15:32:50 -0800

    Added Natspec Comments

--- contracts/ZebraToken.sol
+++ contracts/ZebraToken.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.6.0;
 
 import '@openzeppelin/contracts/token/erc20/ERC20.sol';
 
+
+/**
+    @title ZebraToken
+    @notice The Governance Token behind ZebraSwap
+*/
 contract ZebraToken is ERC20 {
     constructor() ERC20("ZebraToken", "ZBT") public {}
 }
