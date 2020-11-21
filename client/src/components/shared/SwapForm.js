828b74e04c45283912b66e7bd593edbd3e28a50a
Jet <jetjadeja@gmail.com>
2020-11-21 11:19:23 -0800

    Created a Basic SwapForm

--- client/src/components/shared/SwapForm.js
+++ client/src/components/shared/SwapForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Box, Text, Input} from '@chakra-ui/react';
+import {Box, Text, Input, Grid, Button} from '@chakra-ui/react';
 
 /**
  * Remember to remove this once multiple tokens are added
@@ -7,65 +7,87 @@ import {Box, Text, Input} from '@chakra-ui/react';
 import tokenLogo from '../../static/token-logo.png';
 import ethLogo from '../../static/eth-logo.png';
 
-const SwapForm = React.memo(() => {
+const SwapForm = React.memo(({ walletConnected }) => {
     const [amounts, setAmounts] = useState({
         sellToken: 0,
-        buyToken: ""
+        buyToken: 0,
     })
-    return (
-            <Box
-                display="flex"
-                borderRadius={75}
-                borderWidth={0.2}
-                width="400px"
-                height="465px"
-                direction="column"
-                alignItems="center"
 
-                backgroundColor="grey"
+    return (
+        <Box
+            bg="blackAlpha.900"
+            height="375px"
+            width="375px"
+            display="flex"
+            direction="column"
+            alignItems="center"
+            justifyContent="space-around"
+        >
+            <Grid
+                templateColumns="repeat(1, 1fr)"
+                gap={50}
+                display="flex"
+                flexDirection="column"
+                justifyContent="space-between"
+                alignItems="center"
             >
-                <Box>
-                    <Box
-                        display="flex"
-                        borderRadius={75}
-                        borderWidth={0.2}
+                <Box 
+                    display="flex"
+                    direction="column"
+                    paddingLeft={5}
+                    borderWidth="1px" 
+                    width="300px" 
+                    height="75px"
+                    borderRadius={75}
+
+                >
+                    <Input
+                        variant="unstyled"
+                        size="lg"
+                        fontSize="xl"
                         color="white"
-                        width="375px"
-                        height="80px"
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                            <Input
-                                fontSize="160%"
-                                border="none"
-                                outline="none"
-                                focusBorderColor="transparent"
-                            ></Input>
-                    </Box>
-                    <Box
-                        height={200}
+                        placeholder={0.1}
+                        onChange={(event) => {
+                            setAmounts(
+                                {
+                                    sellToken: parseFloat(event.target.value),
+                                    buyToken: parseFloat(event.target.value) * 10
+                                }
+                            )
+                        }}
                     />
-                    <Box
-                        display="flex"
-                        borderRadius={75}
-                        borderWidth={0.2}
-                        color="white"
-                        width="375px"
-                        height="80px"
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                            <Input
-                                fontSize="160%"
-                                border="none"
-                                outline="none"
-                                focusBorderColor="transparent"
-                            ></Input>
-                    </Box>
                 </Box>
-            </Box>
+                <Box
+                    display="flex"
+                    direction="column"
+                    paddingLeft={5}
+                    borderWidth="1px" 
+                    width="300px" 
+                    height="75px"
+                    borderRadius={75}
+                    color="white"
+                >
+                    <Input
+                        size="lg"
+                        variant="unstyled"
+                        fontSize="xl"
+                        placeholder={amounts.buyToken==Number.NaN? 0.0 : amounts.buyToken}
+                        readOnly={true}
+                    />
+                </Box>
+                <Button
+                    width="300px"
+                    height="60px"
+                    borderRadius={50}
+                    bgImage="url(https://i.imgur.com/um4z5xe.png)"
+                    textColor="white"
+                    fontSize="lg"
+                    disabled={!walletConnected}
+                >
+                    {walletConnected? "Swap!" : "Connect Wallet"}
+                </Button>
+            </Grid>
+        </Box>
     )
 });
 
