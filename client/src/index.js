b2a40d8b59208ca00d3d5c895880331fbf5d0c03
Jet <jetjadeja@gmail.com>
2020-11-20 16:31:30 -0800

    Created Index.JS

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { ChakraProvider } from "@chakra-ui/react"
+
+ReactDOM.render(
+    <ChakraProvider>
+    </ChakraProvider>
+    , document.getElementById('root')
+)
