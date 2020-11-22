250ac0c2f542500ac8db2b2d6f9165ae68737fb1
Jet <jetjadeja@gmail.com>
2020-11-22 12:53:25 -0800

    Added Private Update Function to LiquidityPool

--- contracts/ZebraLiquidityPool.sol
+++ contracts/ZebraLiquidityPool.sol
@@ -23,11 +23,31 @@ contract ZebraLiquidityPool is ERC20 {
     uint256 private tokenReserve0;
     uint256 private tokenReserve1;
 
-    ///@dev tokenReserve0 * tokenReserve1
+    ///@dev The timestamp recorded at
+    uint32 private lastUpdateTimestamp;
+
+    ///@dev tokenReserve0 * tokenReserve1 . This is modified everytime liquidity is modified
     uint256 public lastTotalK;
 
-    ///@dev Set the ZebraUnit to the address that deployed this contract.
-    constructor() public {
+    ///@dev Set the ZebraUnit to the address that deployed this contract, and define the tokens
+    constructor(address _token0, address _token1) public {
         unit = msg.sender;
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    ///@dev Return the reserve values for each token
+    function getTokenReserves() public view returns (uint256, uint256) {
+        return (tokenReserve0, tokenReserve1);
+    }
+
+    function _updateTokenReserves(uint256 balance0, uint256 balance1) private {
+        require(balance0 <= uint256(-1) && balance1 <= (-1), "Overflow");
+        uint32 lastTimestamp = uint32(block.timestamp % 2**32);
+        uint32 timeElapsed = lastTimeStamp - lastUpdateTimestamp;
+
+        tokenReserve0 = balance0;
+        tokenReserve1 = balance1;
+        lastUpdateTimestamp = lastTimeStamp;
     }
 }
