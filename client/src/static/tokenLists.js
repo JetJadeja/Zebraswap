aa5d0c2aa55e6b34079eabfab13fe579f97f36b8
Jet <jetjadeja@gmail.com>
2020-11-24 21:00:28 -0800

    Created Token List Information

---
+++ client/src/static/tokenLists.js
@@ -0,0 +1,14 @@
+const tokens = [
+  {
+    name: "Aave Token List",
+    url:
+      "https://bafybeiczexgkep47zswpuay6gq5poc26iffcea7u55d3unj2bm55omgigi.ipfs.dweb.link/",
+    displayUrl: "tokenlist.aave.eth",
+  },
+  {
+    name: "1inch",
+    url:
+      "https://bafybeibc2ppxoy4nautcnkepfbsum5ddufzomqj5derh26tn6yjgcex2qe.ipfs.dweb.link/",
+    displayUrl: "tokens.1inch.eth",
+  },
+];
