a063310e6bff515a1c3b3eeb3509d62d9dcb5c11
Jet <jetjadeja@gmail.com>
2020-11-25 18:10:00 -0800

    Updated tokenList

--- client/src/static/tokenLists.js
+++ client/src/static/tokenLists.js
@@ -1,14 +1,10 @@
-const tokens = [
+const lists = [
   {
-    name: "Aave Token List",
+    name: "Defi",
     url:
       "https://bafybeiczexgkep47zswpuay6gq5poc26iffcea7u55d3unj2bm55omgigi.ipfs.dweb.link/",
     displayUrl: "tokenlist.aave.eth",
   },
-  {
-    name: "1inch",
-    url:
-      "https://bafybeibc2ppxoy4nautcnkepfbsum5ddufzomqj5derh26tn6yjgcex2qe.ipfs.dweb.link/",
-    displayUrl: "tokens.1inch.eth",
-  },
 ];
+
+export default lists;
